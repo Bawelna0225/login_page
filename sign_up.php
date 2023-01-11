58fa895a38c6f70b3f9585e3c2b70338c9766ef3
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2023-01-11 17:23:04 +0100

    removed unused html elements

--- sign_up.php
+++ sign_up.php
@@ -8,17 +8,15 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <div class="form">
-        <form action="signup-user.php" method="POST" autocomplete="">
-            <h2 class="text-center">Signup Form</h2>
-            <p class="text-center">It's quick and easy.</p>
-            <input type="text" name="name" placeholder="Full Name" required >
-            <input type="email" name="email" placeholder="Email Address" required >
-            <input type="password" name="password" placeholder="Password" required>
-            <input type="password" name="cpassword" placeholder="Confirm password" required>
-            <input class="form-control button" type="submit" name="signup" value="Sign Up">
-            <p>Already a member? <a href="login.php">Login here</a></p>
-        </form>
-    </div>
+    <form action="signup-user.php" method="POST" autocomplete="">
+        <h2>Signup Form</h2>
+        <p>It's quick and easy.</p>
+        <input type="text" name="name" placeholder="Full Name" required >
+        <input type="email" name="email" placeholder="Email Address" required >
+        <input type="password" name="password" placeholder="Password" required>
+        <input type="password" name="cpassword" placeholder="Confirm password" required>
+        <input class="button" type="submit" name="signup" value="Sign Up">
+        <p>Already a member? <a href="login.php">Login here</a></p>
+    </form>
 </body>
 </html>
