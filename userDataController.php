42adb39b6e1e8f3bf5d69974bb7b7d63ab664de5
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2023-01-12 20:36:42 +0100

    login user controller

--- userDataController.php
+++ userDataController.php
@@ -39,5 +39,47 @@ if(isset($_POST['signup'])){
         }
     }
 }
+    //Log In
+    if(isset($_POST['login'])){
+        $email = mysqli_real_escape_string($connection, $_POST['email']);
+        $password = mysqli_real_escape_string($connection, $_POST['password']);
+        $check_email = "SELECT * FROM userdata WHERE email = '$email'";
+        $res = mysqli_query($connection, $check_email);
+        if(mysqli_num_rows($res) > 0){
+            $fetch = mysqli_fetch_assoc($res);
+            $fetch_pass = $fetch['password'];
+            if(password_verify($password, $fetch_pass)){
+                $_SESSION['email'] = $email;
+                $_SESSION['password'] = $password;
+                header('location: home.php');
+            }else{
+                $errors['email'] = "Incorrect email or password!";
+            }
+        }else{
+            $errors['email'] = "It's look like you're not yet a member! Click on the bottom link to signup.";
+        }
+    }
 
+
+    // Change password
+    if(isset($_POST['change-password'])){
+        $_SESSION['info'] = "";
+        $password = mysqli_real_escape_string($connection, $_POST['password']);
+        $cpassword = mysqli_real_escape_string($connection, $_POST['cpassword']);
+        if($password !== $cpassword){
+            $errors['password'] = "Confirm password not matched!";
+        }else{
+            $email = $_SESSION['email']; //getting this email using session
+            $encpass = password_hash($password, PASSWORD_BCRYPT);
+            $update_pass = "UPDATE userdata SET password = '$encpass' WHERE email = '$email'";
+            $run_query = mysqli_query($connection, $update_pass);
+            if($run_query){
+                $info = "Your password changed. Now you can login with your new password.";
+                $_SESSION['info'] = $info;
+                header('Location: password-changed.php');
+            }else{
+                $errors['db-error'] = "Failed to change your password!";
+            }
+        }
+    }
 ?>
