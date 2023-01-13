8d4b61bad6473d1121aef7f3581e645ee8a87ad2
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2023-01-13 16:46:19 +0100

    changed password page

---
+++ password-changed.php
@@ -0,0 +1,28 @@
+<?php require_once "userDataController.php"; ?>
+<?php
+if($_SESSION['info'] == false){
+    header('Location: login.php');  
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login Form</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="wrapper">
+        <?php 
+        if(isset($_SESSION['info'])){
+            ?>
+            <div class="alert alert-success">
+            <?php echo $_SESSION['info']; ?>
+            </div>
+            <?php
+        }
+        ?>
+        <a href="home.php">Ok</a>
+    </div>
+</body>
+</html>
