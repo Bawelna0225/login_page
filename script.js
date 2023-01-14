a44dd29069caed0b381a0297ce3895465e48ece6
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2023-01-14 12:07:31 +0100

    toggling dropdown menu with click

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const username = document.querySelector('.username').textContent
 const userLogo = document.querySelector('.user-logo')
+const dropdown = document.querySelector('.dropdown')
 var matches = username.match(/\b(\w)/g);
 var acronym = matches.join('');
 
 userLogo.innerHTML = acronym.toUpperCase()
+
+userLogo.addEventListener('click', () => {
+    console.log('object');
+    dropdown.classList.toggle('open')
+})
