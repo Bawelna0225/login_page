8c61c4eda6571d8bb5b86fa4bd1da19485a618ec
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2023-01-25 11:52:53 +0100

    Create post form & styling

--- createpost.php
+++ createpost.php
@@ -18,9 +18,38 @@ if($email != false && $password != false){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title><?php echo $fetch_info['name'] ?> | Create Post</title>
 </head>
 <body>
-    
+        <form action="createpost.php" method="POST" autocomplete="off">
+        <h2>Create New Post</h2>
+        <?php 
+        if(isset($_SESSION['info'])){
+            ?>
+            <div class="alert alert-success text-center">
+                <?php echo $_SESSION['info']; ?>
+            </div>
+            <?php
+        }
+        ?>
+        <?php
+        if(count($errors) > 0){
+            ?>
+            <div class="alert alert-danger text-center">
+                <?php
+                foreach($errors as $showerror){
+                    echo $showerror;
+                }
+                ?>
+            </div>
+            <?php
+        }
+        ?>
+            <input class="form-control" type="text" name="post_title" placeholder="Post Title" required>    
+            <textarea class="form-control" name="post_content" placeholder="Post Content"></textarea>
+            <input class="form-control button" type="submit" name="change-password" value="Create">
+            <a href="home.php">I changed my mind</a>
+    </form>
 </body>
 </html>
