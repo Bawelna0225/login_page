a8405142740e523210ae4836c4e6bb4d08654c10
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2023-01-25 11:44:18 +0100

    create post button & styling

--- home.php
+++ home.php
@@ -28,7 +28,8 @@ if($email != false && $password != false){
         <a class="logo" href="#">LOGO</a>
         <p class='username'><?php echo $fetch_info['name'] ?></p>
         
-            
+        <div class="buttons">
+            <a href="#">Create Post</a>
             <?php 
                 if($fetch_info['picture'] == '') {
                    echo "<button class='user-logo' id='user-logo'></button>";
@@ -36,7 +37,7 @@ if($email != false && $password != false){
                     echo '<button class="user-logo"><img class="user-logo" src="upload/'. $fetch_info['picture'].'"></button>';
                 }
             ?>
-        
+        </div>
         <div class="dropdown">
             <ul>
                 <li><a href="changepicture.php"><span class="material-symbols-outlined">image</span>Change Profile Picture</a></li>
