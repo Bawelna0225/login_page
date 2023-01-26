ade09c9495cb11211ff2e0dcb28fae9adbe1ef17
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2023-01-26 10:11:03 +0100

    displaying login buttons if user is not logged in

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php require_once "userDataController.php"; ?>
 <?php  
     if (isset($_SESSION['email']) && isset($_SESSION['password'])) {
-        echo  $_SESSION['email'];
+        $isUserLoggedIn = true;
     } else {
-        echo "nima sysji";
+        $isUserLoggedIn = false;
     }
 ?>
 <!DOCTYPE html>
@@ -15,6 +15,39 @@
     <title>Document</title>
 </head>
 <body>
-
+    <nav>
+        <a class="logo" href="#">LOGO</a>
+        <?php
+            if ($isUserLoggedIn) {
+                ?>
+                <p class='username'><?php echo $fetch_info['name'] ?></p>
+                <div class="buttons">
+                    <a href="createpost.php">Create Post</a>
+                    <?php 
+                        if($fetch_info['picture'] == '') {
+                        echo "<button class='user-logo' id='user-logo'></button>";
+                        } else {
+                            echo '<button class="user-logo"><img class="user-logo" src="upload/'. $fetch_info['picture'].'"></button>';
+                        }
+                    ?>
+                </div>
+                <div class="dropdown">
+                    <ul>
+                        <li><a href="changepicture.php"><span class="material-symbols-outlined">image</span>Change Profile Picture</a></li>
+                        <li><a href="changepassword.php"><span class="material-symbols-outlined">lock_reset</span>Change Your Password</a></li>
+                        <li><a href="logout.php"><span class="material-symbols-outlined">logout</span> Log out</a></li>
+                    </ul>
+                </div>
+        <?php
+            } else {
+                echo '
+                <div class="buttons">
+                    <a href="login.php">Log In</a>
+                    <a href="sign_up.php">Sign Up</a>
+                </div>';
+            } 
+        ?>
+    </nav>
+    <section></section>
 </body>
 </html>
