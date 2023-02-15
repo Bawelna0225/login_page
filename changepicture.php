356f03de3e6ab753783e2f146db1f6babf4f18da
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2023-02-15 13:21:22 +0100

    viewport fix

--- changepicture.php
+++ changepicture.php
@@ -17,6 +17,8 @@ if($email != false && $password != false){
 <html>
 <head>
     <title>Change Picture</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
     <link rel="stylesheet" href="css/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
